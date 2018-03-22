87b10e720032fb9bec61d6d0a8027fbd3a2c5d88
Anton Litvinov <alitvinov@justcoded.co>
2018-03-22 19:03:04 +0200

    done stage 3

--- templater.js
+++ templater.js
@@ -14,23 +14,66 @@
 
 // Stage 2
 
+// var Templater = {
+//     tagRepo: [],
+//     tmplRepo: [],
+//     addTag: function(tag, template) {
+//         this.tagRepo.push(tag);
+//         this.tmplRepo.push(template)
+//     },
+//     run: function() {
+//         var allElements;
+//         for (var i = 0; i < this.tagRepo.length; i++) {
+//             allElements = document.getElementsByTagName(this.tagRepo[i]);
+//             var HtmlArr = Array.from(allElements);
+//             console.log(HtmlArr);
+//             for (var j = 0; j < HtmlArr.length; j++) {
+//                 HtmlArr[j].outerHTML = (this.tmplRepo[i]);
+//                 console.log(HtmlArr[j]);
+//             }
+//         }
+//     }
+// }
+
+// Stage 3
+
 var Templater = {
-    tagRepo: [],
-    tmplRepo: [],
+    tagRepo: [ ],
+    tmplRepo: [ ],
+    ElementsArr: [ ],
     addTag: function(tag, template) {
-        this.tagRepo.push(tag);
-        this.tmplRepo.push(template)
+      this.tagRepo.push(tag);
+      this.tmplRepo.push(template)
     },
     run: function() {
+        
         var allElements;
+
         for (var i = 0; i < this.tagRepo.length; i++) {
+          
+            var element = [ [ ],[ ],[ ] ];
+            this.ElementsArr.push(element);
             allElements = document.getElementsByTagName(this.tagRepo[i]);
             var HtmlArr = Array.from(allElements);
-            console.log(HtmlArr);
+          
             for (var j = 0; j < HtmlArr.length; j++) {
-                HtmlArr[j].outerHTML = (this.tmplRepo[i]);
-                console.log(HtmlArr[j]);
+              var types = HtmlArr[j].getAttribute('type');
+              var classes = HtmlArr[j].getAttribute('class');
+              var text = HtmlArr[j].innerHTML;
+              element[0].push(types);
+              element[1].push(classes);
+              element[2].push(text);
+              var newTemplate = this.render(this.tmplRepo[i],this.ElementsArr[i],i,j);
+              HtmlArr[j].outerHTML = (newTemplate);
+
             }
-        }
-    }
+        }; 
+    },
+    render: function(template, element, counteri, counterj) {
+      var RegExpArr = ['{{type}}','{{class}}','{{html}}'];
+      for (var i = 0; i < RegExpArr.length; i++) {
+        template = template.replace(new RegExp(RegExpArr[i]),this.ElementsArr[counteri][i][counterj]);
+      }
+      return template;
+    },
 }
